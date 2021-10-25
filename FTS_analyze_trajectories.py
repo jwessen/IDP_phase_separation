c6c6ba4a1f1f2e9bf9062d6e319b2a5a15933346
jwessen <jonas.wessen@hotmail.com>
2021-10-25 15:53:27 -0400

    Add files via upload

---
+++ FTS_analyze_trajectories.py
@@ -0,0 +1,212 @@
+###########################################################################
+# This code analyzes the trajectory files produced by the accompanying    #
+# field theory simulation (FTS) code "FTS_trajectories_MPI.py". For each  #
+# temperature/Bjerrum length, the thermal average of the chemical         #
+# potential (mu) and osmotic pressure (Pi) are computed as a function of  #
+# polymer bead density, and written to a file ending with "_mu_Pi.txt".   #
+# The self-intersection points of these (mu(rho),Pi(rho)) curves are then #
+# numerically found, which are used to construct the binodal curves that  #
+# constitute the FTS phase diagrams.                                      #
+#                                                                         #
+# The code is used in the publication                                     #
+#                                                                         #
+#    Lin Y, Wessén J, Pal T, Das S & Chan H S, XXXXX (2021)               #
+#                                                                         #
+# and follows the methods described therein.                              #
+###########################################################################
+
+import numpy as np 
+import scipy.optimize as opt
+from scipy import interpolate
+from scipy.signal import savgol_filter
+from sympy.geometry import Point, Segment, intersection
+
+# Set show_plot = True if you wish to monitor the search for
+# co-existence points by plotting the (mu(rho),Pi(rho)) curves 
+# along with the found intersection points.
+show_plot = True
+if show_plot:
+    import matplotlib.pyplot as plt
+
+###############################
+######### Functions ###########
+###############################
+
+# Returns the differences in chemical potential and osmotic pressure for two
+# densities contained in the array x = [ log(rho1), log(rho2) ]. This function
+# is used for numerical solution of the phase equilibrium conditions.
+def diff_func(x, rho, mu, Pi):
+    rho_bulk = np.exp(x)
+    rho_L = np.min(rho_bulk)
+    rho_H = np.max(rho_bulk)
+
+    if rho_L < rho[0] or rho_H > rho[-1]:
+        return [1e5, 1e5]
+
+    mu_int = interpolate.interp1d(rho,mu)
+    Pi_int = interpolate.interp1d(rho,Pi)
+
+    mu_diff = mu_int(rho_L) - mu_int(rho_H)
+    Pi_diff = Pi_int(rho_L) - Pi_int(rho_H)
+
+    return [ mu_diff , Pi_diff ]
+
+def make_segment(Pi1,mu1,Pi2,mu2):
+    p1,p2 = Point(Pi1, mu1, evaluate=False) , Point(Pi2, mu2, evaluate=False)
+    return Segment(p1,p2, evaluate=False)
+
+# Returns the bulk densities, chemical potential and osmotic pressure of the self-intersection point
+# of the input {mu(rho), Pi(rho)} curve. If no satisfactory self-intersection point is found, the
+# array [-1] is returned. 
+def find_intersection(rho, mu, Pi):
+    # Remove crashed runs (if any)
+    indices = np.array([str(m)!='nan' for m in mu])
+    rho = rho[indices]
+    mu  = mu[indices]
+    Pi  = Pi[indices]
+
+    if show_plot:
+        plt.plot(mu,Pi,'o',ms=4)
+        plt.xlabel("chemical potential")
+        plt.ylabel("osmotic pressure")
+
+    # Slightly smooth the data. This makes the calculation more robust against
+    # statistical fluctuations in the vicinity of the self-intersection point.
+    mu = savgol_filter(mu, 11, 9) # window size 11, polynomial order 9
+    Pi = savgol_filter(Pi, 11, 9)
+    
+    if show_plot:
+        plt.plot(mu,Pi,'-')
+
+    # The following looks for approximate solutions to mu(rho1)=mu(rho2), Pi(rho1)=Pi(rho2)
+    # by turning the (mu(rho),Pi(rho)) curve into sympy.geometry Segments, and checking if 
+    # each pair of segments intersect using the sympy intersection function. 
+    print("Looking for approximate intersections...")
+    segments = np.array([ make_segment( Pi[1],mu[1],Pi[0],mu[0] ) ,\
+                          make_segment( Pi[2],mu[2],Pi[1],mu[1] ) ])
+    rho_Hs = np.array([])
+    rho_Ls = np.array([])
+    intersection_found = False
+    for i in range(3,len(rho)):
+        if i%10==0: print(len(rho)-i)
+        s1 = make_segment( Pi[i] , mu[i] , Pi[i-1] ,mu[i-1] ) 
+        for j in range(len(segments)-1):
+            s2 = segments[j]        
+            intersec = s1.intersection(s2)
+            if len(intersec) > 0:
+                print("Intersection found.")
+                intersection_found = True
+                rho_Hs = np.append(rho_Hs, (rho[i]+rho[i-1])/2. )
+                rho_Ls = np.append(rho_Ls, (rho[j]+rho[j-1])/2. )
+        segments = np.append(segments,s1)
+    
+    if not intersection_found:
+        print("Found no intersection.")
+        if show_plot:
+            plt.show()
+            plt.close()
+        return np.array([-1])
+
+    # Each approximate intersection is now used as an initial guess for the scipy.optimization
+    # function fsolve to find more accurate solutions to mu(rho1)=mu(rho2), Pi(rho1)=Pi(rho2).
+    rho_H_final = np.array([])
+    rho_L_final = np.array([])
+    print("Using approximate intersections as inital guesses...")
+    for i in range(len(rho_Hs)):
+        rho_sol = opt.fsolve( diff_func , [np.log( rho_Ls[i] ) , np.log( rho_Hs[i] )] , args=(rho,mu,Pi) )
+        final_diff = diff_func(rho_sol, rho,Pi,mu)
+        rho_sol = np.exp(rho_sol)
+
+        rho_L = np.min(rho_sol)
+        rho_H = np.max(rho_sol)
+
+        if np.abs(rho_L - rho_H) <= 1e-5:
+            print("Discarding solution, bulk densities too similar!")
+        elif np.sum(np.abs(final_diff)) >= 1e-5:
+            print("Discarding solution, covergence not good enough!")
+        else:
+            rho_H_final = np.append(rho_H_final, rho_H )
+            rho_L_final = np.append(rho_L_final, rho_L )
+
+    if len(rho_H_final) == 0:
+        print("All intersections were discarded.")
+        if show_plot:
+            plt.show()
+            plt.close()
+        return np.array([-1])
+
+    mu_int = interpolate.interp1d(rho,mu)
+    Pi_int = interpolate.interp1d(rho,Pi)
+
+    mu_c = np.array( [mu_int(r) for r in rho_H_final] )
+    Pi_c = np.array( [Pi_int(r) for r in rho_H_final] )
+
+    # Plot all found self-intersection points.
+    if show_plot:
+        plt.plot(mu_c,Pi_c,'*', color='red', ms=8)
+
+    # Unphysical self-intersection points are sometimes found around the spinodal turning
+    # points (these tend to disappear with larger system sizes). The binodal self-intersection
+    # points correspond to a larger difference between the high- and low density bulk phases. 
+    # The solution with the largest density difference is therefore deemed the physical one. 
+    # However, it is recommended visually verify this by plotting all found self-intersection points. 
+    density_diffs = rho_H_final - rho_L_final
+    index = np.argmax(density_diffs)
+
+    rho_L_out = rho_L_final[index]
+    rho_H_out = rho_H_final[index]
+    mu_c_out  = mu_c[index]
+    Pi_c_out  = Pi_c[index] 
+
+    if show_plot:
+        plt.plot(mu_c_out,Pi_c_out,'o', color='green', ms=10, fillstyle='none')
+        plt.show()
+        plt.close()
+
+    return np.array( [ rho_L_out, rho_H_out, mu_c_out, Pi_c_out ])
+
+##########################
+######### Main ###########
+##########################
+
+ncpus = 40
+all_rho = np.exp( np.linspace( np.log(1e-6), np.log(10), ncpus ) )  # All bead bulk densities considered. 
+all_lB  = 1. / np.linspace(1.,6.,5)                                 # All values of the Bjerrum length (lB). 
+
+t_equil = 50. # Equilibration Complex Langevin time 
+
+run_label_base = "data/sv20"
+with open(run_label_base + "_phase_diagram.txt", 'w') as pd_file:
+
+    # Calculate thermally averaged chemical potentials and osmotic pressures from FTS trajectories
+    for lB_ind in range(len(all_lB)):
+
+        mu_av = np.zeros( ncpus , dtype=complex)
+        Pi_av = np.zeros( ncpus , dtype=complex)
+        for run in range(ncpus):
+            # Read the trajectory file
+            _, t, mu_R, mu_I, Pi_R, Pi_I = np.loadtxt( run_label_base+"_lB_"+str(lB_ind)+"_"+str(run)+"_traj.txt" ).T
+
+            # Remove equilibration period
+            prod_indices = t > t_equil
+            mu_R = mu_R[ prod_indices ]
+            mu_I = mu_I[ prod_indices ]
+            Pi_R = Pi_R[ prod_indices ]
+            Pi_I = Pi_I[ prod_indices ]
+
+            # Compute Complex Langevin time averages
+            mu_av[run] = np.mean( mu_R + 1j * mu_I )
+            Pi_av[run] = np.mean( Pi_R + 1j * Pi_I )
+
+        # Save mu(rho) and Pi(rho) to file. 
+        out_data = np.array( [all_rho, mu_av.real, mu_av.imag, Pi_av.real, Pi_av.imag] )
+        np.savetxt( run_label_base+"_lB_"+str(lB_ind)+"_mu_Pi.txt" , out_data.T )
+
+        # Find bulk densities given by the self-intersection of the ( mu(rho), Pi(rho) ) curve
+        sol = find_intersection(all_rho, mu_av.real, Pi_av.real )
+        # Write binodal curve to file
+        if len(sol) == 4:
+            rho_L, rho_H , mu_c, Pi_c = sol
+
+            pd_file.write('{:.8e} {:.8e} {:.8e} {:.8e} {:.8e}'.format(all_lB[lB_ind], rho_L, rho_H, mu_c, Pi_c) )                 
+            pd_file.write('\n')
---
+++ FTS_trajectories_MPI.py
@@ -0,0 +1,89 @@
+###########################################################################
+# Field theoretic simulation (FTS) code for generating Complex Langevin   #
+# time evolution trajectories of the chemical potential and osmotic       #
+# pressure of a polyampholyte solution. The script utilises parallel      #
+# processing and is suitable e.g. for running on a cluster.               #
+#                                                                         #
+# The code is used in the publication                                     #
+#                                                                         #
+#    Lin Y, Wessén J, Pal T, Das S & Chan H S, XXXXX (2021)               #
+#                                                                         #
+# and follows the methods described therein.                              #
+###########################################################################
+
+from FTS_polyampholytes import *
+import CL_seq_list as sl
+import multiprocessing as mp
+import sys
+
+def exe( PS , run_label ):
+
+    np.random.seed()
+
+    # CL time step
+    dt      = 0.01       # Complex-Langevin time step
+    t_prod  = int(5e4)   # Total number of time steps
+
+    # Store parameter values
+    params = PS.get_params()
+    with open( run_label + "_params.txt" , 'w') as f:
+        print(params, file=f)
+
+    # Initialize fields as random fluctuations around mean field solution
+    init_size = 0.1
+    w   = init_size * ( np.random.randn( PS.Nx,PS.Nx,PS.Nx ) + \
+                   1j * np.random.randn( PS.Nx,PS.Nx,PS.Nx ) )
+    psi = init_size * ( np.random.randn( PS.Nx,PS.Nx,PS.Nx ) + \
+                   1j * np.random.randn( PS.Nx,PS.Nx,PS.Nx ) )
+
+    w   -= np.mean(w) + 1j * PS.rhop0 * PS.v
+    psi -= np.mean(psi)
+    PS.set_fields(w,psi)
+
+    # Compute the M^(-1) matrix used for semi-implicit Complex Langevin evolution
+    Minv = get_M_inv( PS, dt)
+
+    with open( run_label + "_traj.txt" , 'w') as ev_file:
+        # Generate Complex Langevin evolution trajectory for the chemical potential and osmotic pressure
+        for t in range(t_prod):
+            # Sample every 50th step
+            if t%50 == 0:
+                mu = PS.get_chem_pot()
+                Pi = PS.get_pressure()
+
+                print(t, mu.real , mu.imag, Pi.real, Pi.imag) 
+                    
+                ev_file.write('{:.8e} {:.8e} {:.8e} {:.8e} {:.8e} {:.8e}'.format(t, t*dt, mu.real, mu.imag, Pi.real, Pi.imag  ) )                 
+                ev_file.write('\n')
+                ev_file.flush()
+            CL_step_SI(PS, Minv, dt, useSI=True)
+
+
+if __name__ == '__main__':
+    ncpus = 40
+    all_rho = np.exp( np.linspace( np.log(1e-6), np.log(10) , ncpus ) ) # All bead bulk densities considered. 
+
+    all_lB = 1. / np.linspace(1.,6.,5) # All values of the Bjerrum length (lB)
+    lB_index = int(sys.argv[1])        # Index of the lB value for this run
+
+    lB = all_lB[lB_index]    # Bjerrum length (inverse of the reduced temperature)
+    v  = 0.0068              # Excluded volume parameter
+    a  = 1./np.sqrt(6.)      # Smearing length
+
+    sig, N, the_seq = sl.get_the_charge("sv20")
+
+    run_label_base = "data/sv20_lB_"+str(lB_index)+"_"
+    
+    procs = []
+    for run in range(ncpus):
+        run_label = run_label_base + str(run)
+        PS = PolySol(sig, all_rho[run], lB, v, a, Nx=24)
+
+        proc = mp.Process(target=exe, args=( PS , run_label , ) ) 
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
+
+
