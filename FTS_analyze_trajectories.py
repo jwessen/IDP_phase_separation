67b3717e5cdbf354916b0e88b3668192d7c74744
jwessen <jonas.wessen@hotmail.com>
2021-12-06 20:48:20 -0500

    Update FTS_analyze_trajectories.py

--- FTS_analyze_trajectories.py
+++ FTS_analyze_trajectories.py
@@ -10,7 +10,11 @@
 #                                                                         #
 # The code is used in the publication                                     #
 #                                                                         #
-#    Lin Y, Wessén J, Pal T, Das S & Chan H S, XXXXX (2021)               #
+#    Y.-H. Lin, J. Wessén, T. Pal, S. Das and H.S. Chan (2021)            #
+#    Numerical Techniques for Applications of Analytical Theories to      #
+#    Sequence-Dependent Phase Separations of Intrinsically Disordered     #
+#    Proteins. Methods in Molecular Biology (Springer-Nature),            #
+#    accepted for publication.                                            #
 #                                                                         #
 # and follows the methods described therein.                              #
 ###########################################################################
